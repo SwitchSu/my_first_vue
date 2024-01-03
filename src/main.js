49a7b0164ef3588e35fbcb6a32a81f7410fa0d8a
SwitchSu <152354849+SwitchSu@users.noreply.github.com>
2024-01-03 17:05:24 +0800

    time

--- src/main.js
+++ src/main.js
@@ -8,9 +8,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faShoppingCart, faInstagram, faFacebookSquare, fas)
 
 const app = createApp(App)
 
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon) //使用kebab-case
 app.mount('#app')
--- src/views/ToDoListView.vue
+++ src/views/ToDoListView.vue
@@ -1,12 +1,56 @@
 <script>
 export default {
     data() {
-
+        return {
+            addText: '',
+            toDoArr: [
+                {
+                    id: 1,
+                    todo: '吃午餐',
+                    checkList: false,
+                    lastTime: '',
+                    recordTime: '',
+                },
+            ],
+            deadline: '',
+            record: '',
+        }
     },
     methods: {
-
+        addList() {
+            if (!this.addText) return;
+            const listId = this.toDoArr.length ? Math.max(...this.toDoArr.map(item => item.id)) + 1 : 1;
+            // const listId = this.toDoArr.length += 1;
+            this.toDoArr.push({
+                id: listId,
+                todo: this.addText,
+                checkList: false,
+                lastTime: this.deadline,
+                recordTime: this.record,
+            })
+            // console.log(this.toDoArr.length);
+            this.addText = '';
+            sessionStorage.setItem('todoList', JSON.stringify(this.toDoArr));
+        },
+        removeList(id) {
+            this.toDoArr.remove({
+                id: this.toDoArr.id,
+                todo: this.addText,
+                checkList: false,
+                lastTime: this.deadline,
+                recordTime: this.record,
+            })
+            sessionStorage.removeItemItem('todoList', JSON.stringify(this.toDoArr));
+            sessionStorage.setItem('todoList', JSON.stringify(this.toDoArr));
+        }
     },
     mounted() {
+        if (sessionStorage.getItem('todoList')) {
+            this.toDoArr = JSON.parse(sessionStorage.getItem('todoList'));
+        }
+        // else {
+        //     sessionStorage.setItem('todoList', JSON.stringify(this.toDoArr));
+        // }
 
     },
 }
@@ -16,26 +60,38 @@ export default {
     <main class="border-[gray] border-[1px] p-4">
         <h1>Todo List</h1>
         <div class="bg-[#8DD7CF] border-[#1AAE9F] border-[1px] p-2">
-            <input type="text" class="text-[#C3CFD9] border-[gray] border-[1px] px-2 py-1 mr-[20px]" value="Add New Todo Here...">
-            <button type="button" class="add text-[#44BDB1] bg-[white] border-[#63C8BD] border-[1px] rounded-md px-4 py-1">Add</button>
+            <input v-model="addText" type="text" class="border-[gray] border-[1px] px-2 py-1 mr-2"
+                placeholder="Add New Todo Here...">
+            <span>deadline:<input type="date" v-model="deadline"></span>
+            <span>recording:<input type="date" v-model="record"></span>
+            <button type="button" @click="addList()"
+                class="text-[#44BDB1] bg-[white] border-[#63C8BD] border-[1px] rounded-md px-3 py-1 ml-2">
+                <font-awesome-icon :icon="['fas', 'plus']" />
+            </button>
         </div>
         <hr class="text-[#DFE6ED] my-2">
-        <div class=" border-[gray] border-[1px] p-2">
-            <div class="bg-[#E9A2AD] border-[#D3455B] border-[1px] px-1 py-3">
-                <input type="checkbox" class="mr-[10px]">
-                <input type="text" class="text-[#C3CFD9] border-[gray] border-[1px] p-1 mr-[10px]"
-                    value=".todo__title">
-                <button type="button" class="add text-[#D65065] bg-[white] border-[#E18190] border-[1px] rounded-md px-2 py-1">Delete</button>
+        <div class="h-[250px] border-[gray] border-[1px] p-2 overflow-y-scroll">
+            <div v-for="item in toDoArr" :key="item.id"
+                class="bg-[#E9A2AD] border-[#D3455B] border-[1px] px-1 py-2 my-1 flex justify-between items-center">
+                <input v-model="item.checkList" type="checkbox" class="mr-[10px]">
+                <!-- <input type="text" class="border-[gray] border-[1px] p-1 mr-[10px]" placeholder=".todo__title"> -->
+                <span :class="{ 'line-through': item.checkList }">{{ item.todo }}</span>
+                <span>最後時間:{{ item.lastTime }}</span>
+                <span>紀錄時間:{{ item.recordTime }}</span>
+                <button type="button" class="text-[#D65065] bg-[white] border-[#E18190] border-[1px] rounded-md px-2 py-1" @click="removeList(item.id)">
+                    <font-awesome-icon :icon="['fas', 'trash']" />
+                </button>
             </div>
-            <div class="bg-[#E9A2AD] border-[#D3455B] border-[1px] px-1 py-3">
+            <!-- <div class="bg-[#E9A2AD] border-[#D3455B] border-[1px] px-1 py-3">
                 <input type="checkbox" class="mr-[10px]">
-                <input type="text" class="text-[#C3CFD9] border-[gray] border-[1px] p-1 mr-[10px]"
-                    value=".todo__title">
-                <button type="button" class="add text-[#D65065] bg-[white] border-[#E18190] border-[1px] rounded-md px-2 py-1">Delete</button>
-            </div>
+                <input type="text" class="border-[gray] border-[1px] p-1 mr-[10px]" placeholder=".todo__title">
+                <button type="button"
+                    class="add text-[#D65065] bg-[white] border-[#E18190] border-[1px] rounded-md px-2 py-1">
+                    <font-awesome-icon :icon="['fas', 'trash']" />
+                </button>
+            </div> -->
         </div>
     </main>
     <footer></footer>
 </template>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
